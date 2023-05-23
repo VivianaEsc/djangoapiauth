c4d5741d02e3c8c2a278e6a0f8ee12d4aafaf765
unknown <Administrador@RFR.LOCAL>
2023-05-23 13:27:53 -0500

    Cambios para AlmaLinux

--- proyectoAuth/settings.py
+++ proyectoAuth/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,19 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-q_zrjg6x^)s3zg5winf*ra%$&n#y2#c0aojuj*c$5z_q!^m!=5'
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = 'django-insecure-q_zrjg6x^)s3zg5winf*ra%$&n#y2#c0aojuj*c$5z_q!^m!=5'
+#SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
+#DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['186.4.241.122']
 
+"""
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+"""
 
 # Application definition
 
@@ -80,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'proyectoAuth.wsgi.application'
+#WSGI_APPLICATION = 'proyectoAuth.wsgi.application'
 
 
 # Database
@@ -95,11 +96,22 @@ WSGI_APPLICATION = 'proyectoAuth.wsgi.application'
     }
 }"""
 
+# DATABASES = { 
+#     'default': dj_database_url.config(
+#         default = 'sqlite:///db.sqlite3',
+#         conn_max_age = 600
+#     )
+# }
+
 DATABASES = { 
-    'default': dj_database_url.config(
-        default = 'sqlite:///db.sqlite3',
-        conn_max_age = 600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'api_auth',
+        'USER': 'postgres',
+        'PASSWORD': 'djangoapiauth',
+        'HOST': 'apiauth.cayoxgnlehvr.sa-east-1.rds.amazonaws.com',
+        'PORT': '5432'
+    }
 }
 
 
@@ -138,10 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-if not DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:    
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -153,5 +166,5 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:4200'
+    'http://186.4.241.122:4200'
 ]
